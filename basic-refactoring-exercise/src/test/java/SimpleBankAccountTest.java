5b75694942609b0ba3094561ad83cda2ac8b4852
diego-coli <diego.coli@studio.unibo.it>
2025-02-24 12:00:24 +0100

    refactor: magic numbers, methods names,

--- basic-refactoring-exercise/src/test/java/SimpleBankAccountTest.java
+++ basic-refactoring-exercise/src/test/java/SimpleBankAccountTest.java
@@ -10,17 +10,16 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class SimpleBankAccountTest {
 
+    public static final int INITIAL_BALANCE = 0;
     public static final int NOT_EXISTING_USER_ID = 2;
-    public static final int FEE_1$ = 1;
+    public static final int EXPECTED_AMOUNT = 100;
+    public static final int DEPOSIT_AMOUNT = 100;
+    public static final int DEPOSIT_AMOUNT_NOT_EXISTING_USER = 50;
+    public static final int WITHDRAW_AMOUNT = 70;
+    public static final int WITHDRAW_TOO_HIGH_AMOUNT = 100;
+    public static final int EXPECTED_AMOUNT_AFTER_WITHDRAW = 29;
     private AccountHolder accountHolder;
     private BankAccount bankAccount;
-    public static final int INITIAL_BALANCE = 0;
-    public static final int MONEY_100$ = 100;
-    public static final int MONEY_70$ = 70;
-    public static final int MONEY_50$ = 50;
-    public static final int EXPECTED_100$ = 100;
-    public static final int EXPECTED_29$ = 29;
-    public static final int EXPECTED_30$ = 30;
 
     @BeforeEach
     void initAccount(){
@@ -38,35 +37,35 @@ class SimpleBankAccountTest {
 
     @Test
     void deposit() {
-        bankAccount.deposit(accountHolder.getId(), MONEY_100$);
-        assertEquals(EXPECTED_100$, bankAccount.getBalance());
+        bankAccount.deposit(accountHolder.getId(), DEPOSIT_AMOUNT);
+        assertEquals(EXPECTED_AMOUNT, bankAccount.getBalance());
     }
 
     @Test
     void wrongDeposit() {
-        bankAccount.deposit(accountHolder.getId(), MONEY_100$);
-        bankAccount.deposit(NOT_EXISTING_USER_ID, MONEY_50$);
-        assertEquals(EXPECTED_100$, bankAccount.getBalance());
+        bankAccount.deposit(accountHolder.getId(), DEPOSIT_AMOUNT);
+        bankAccount.deposit(NOT_EXISTING_USER_ID, DEPOSIT_AMOUNT_NOT_EXISTING_USER);
+        assertEquals(EXPECTED_AMOUNT, bankAccount.getBalance());
     }
 
     @Test
     void withdraw() {
-        bankAccount.deposit(accountHolder.getId(), MONEY_100$);
-        bankAccount.withdraw(accountHolder.getId(), MONEY_70$);
-        assertEquals(EXPECTED_29$, bankAccount.getBalance());
+        bankAccount.deposit(accountHolder.getId(), DEPOSIT_AMOUNT);
+        bankAccount.withdraw(accountHolder.getId(), WITHDRAW_AMOUNT);
+        assertEquals(EXPECTED_AMOUNT_AFTER_WITHDRAW, bankAccount.getBalance());
     }
 
     @Test
     void wrongUserWithdraw() {
-        bankAccount.deposit(accountHolder.getId(), MONEY_100$);
-        bankAccount.withdraw(NOT_EXISTING_USER_ID, MONEY_70$);
-        assertEquals(EXPECTED_100$, bankAccount.getBalance());
+        bankAccount.deposit(accountHolder.getId(), DEPOSIT_AMOUNT);
+        bankAccount.withdraw(NOT_EXISTING_USER_ID, WITHDRAW_AMOUNT);
+        assertEquals(EXPECTED_AMOUNT, bankAccount.getBalance());
     }
 
     @Test
     void notEnoughMoneyWithdraw() {
-        bankAccount.deposit(accountHolder.getId(), MONEY_100$);
-        bankAccount.withdraw(accountHolder.getId(), MONEY_100$);
-        assertEquals(EXPECTED_100$, bankAccount.getBalance());
+        bankAccount.deposit(accountHolder.getId(), DEPOSIT_AMOUNT);
+        bankAccount.withdraw(accountHolder.getId(), WITHDRAW_TOO_HIGH_AMOUNT);
+        assertEquals(EXPECTED_AMOUNT, bankAccount.getBalance());
     }
 }
