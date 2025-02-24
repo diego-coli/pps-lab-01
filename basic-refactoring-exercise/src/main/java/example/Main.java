c710ea3fd746e1e4404aa21b932bf3a5fdfb7f2c
floyddc <diegocoli68@gmail.com>
2025-02-24 10:45:00 +0100

    Magic numbers e nomi metodi

--- basic-refactoring-exercise/src/main/java/example/Main.java
+++ basic-refactoring-exercise/src/main/java/example/Main.java
@@ -6,14 +6,22 @@ import example.model.SimpleBankAccount;
 
 public class Main {
 
+    public static final int INITIAL_BALANCE = 0;
+    public static final int MONEY_100$ = 100;
+    public static final int MONEY_80$ = 80;
+    public static final int MONEY_30$ = 30;
+
     public static void main(String[] args) {
         final AccountHolder accountHolder = new AccountHolder("Mario", "Rossi", 1);
-        final BankAccount bankAccount = new SimpleBankAccount(accountHolder, 0);
-        bankAccount.deposit(accountHolder.getId(), 100);
+        final BankAccount bankAccount = new SimpleBankAccount(accountHolder, INITIAL_BALANCE);
+
+        bankAccount.deposit(accountHolder.getId(), MONEY_100$);
         System.out.println("Current balance is " + bankAccount.getBalance());
-        bankAccount.withdraw(accountHolder.getId(), 30);
+
+        bankAccount.withdraw(accountHolder.getId(), MONEY_30$);
         System.out.println("Current balance is " + bankAccount.getBalance());
-        bankAccount.withdraw(accountHolder.getId(), 80);
+
+        bankAccount.withdraw(accountHolder.getId(), MONEY_80$);
         System.out.println("Current balance is " + bankAccount.getBalance());
     }
 }
--- basic-refactoring-exercise/src/test/java/SimpleBankAccountTest.java
+++ basic-refactoring-exercise/src/test/java/SimpleBankAccountTest.java
@@ -10,44 +10,51 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class SimpleBankAccountTest {
 
+    public static final int NOT_EXISTING_USER_ID = 2;
     private AccountHolder accountHolder;
     private BankAccount bankAccount;
+    public static final int INITIAL_BALANCE = 0;
+    public static final int MONEY_100$ = 100;
+    public static final int MONEY_70$ = 70;
+    public static final int MONEY_50$ = 50;
+    public static final int EXPECTED_100$ = 100;
+    public static final int EXPECTED_30$ = 30;
 
     @BeforeEach
-    void beforeEach(){
+    void initAccount(){
         accountHolder = new AccountHolder("Mario", "Rossi", 1);
-        bankAccount = new SimpleBankAccount(accountHolder, 0);
+        bankAccount = new SimpleBankAccount(accountHolder, INITIAL_BALANCE);
     }
 
     @Test
-    void testInitialBalance() {
-        assertEquals(0, bankAccount.getBalance());
+    void initialBalance() {
+        assertEquals(INITIAL_BALANCE, bankAccount.getBalance());
     }
 
     @Test
-    void testDeposit() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        assertEquals(100, bankAccount.getBalance());
+    void deposit() {
+        bankAccount.deposit(accountHolder.getId(), MONEY_100$);
+        assertEquals(EXPECTED_100$, bankAccount.getBalance());
     }
 
     @Test
-    void testWrongDeposit() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        bankAccount.deposit(2, 50);
-        assertEquals(100, bankAccount.getBalance());
+    void wrongDeposit() {
+        bankAccount.deposit(accountHolder.getId(), MONEY_100$);
+        bankAccount.deposit(NOT_EXISTING_USER_ID, MONEY_50$);
+        assertEquals(EXPECTED_100$, bankAccount.getBalance());
     }
 
     @Test
-    void testWithdraw() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        bankAccount.withdraw(accountHolder.getId(), 70);
-        assertEquals(30, bankAccount.getBalance());
+    void withdraw() {
+        bankAccount.deposit(accountHolder.getId(), MONEY_100$);
+        bankAccount.withdraw(accountHolder.getId(), MONEY_70$);
+        assertEquals(EXPECTED_30$, bankAccount.getBalance());
     }
 
     @Test
-    void testWrongWithdraw() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        bankAccount.withdraw(2, 70);
-        assertEquals(100, bankAccount.getBalance());
+    void wrongWithdraw() {
+        bankAccount.deposit(accountHolder.getId(), MONEY_100$);
+        bankAccount.withdraw(NOT_EXISTING_USER_ID, MONEY_70$);
+        assertEquals(EXPECTED_100$, bankAccount.getBalance());
     }
 }
