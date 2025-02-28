3ddaaf926950f33f402ed0955f772a9b1139a6cb
diego-coli <diego.coli@studio.unibo.it>
2025-02-28 14:56:28 +0100

    CircularQueue interface and class implementation and tests creation

--- basic-refactoring-exercise/src/main/java/example/Main.java
+++ basic-refactoring-exercise/src/main/java/example/Main.java
@@ -7,21 +7,21 @@ import example.model.SimpleBankAccount;
 public class Main {
 
     public static final int INITIAL_BALANCE = 0;
-    public static final int MONEY_100$ = 100;
-    public static final int MONEY_80$ = 80;
-    public static final int MONEY_30$ = 30;
+    public static final int DEPOSIT = 100;
+    public static final int WITHDRAW = 80;
+    public static final int WITHDRAW_TOO_HIGH = 30;
 
     public static void main(String[] args) {
         final AccountHolder accountHolder = new AccountHolder("Mario", "Rossi", 1);
         final BankAccount bankAccount = new SimpleBankAccount(accountHolder, INITIAL_BALANCE);
 
-        bankAccount.deposit(accountHolder.getId(), MONEY_100$);
+        bankAccount.deposit(accountHolder.getId(), DEPOSIT);
         System.out.println("Current balance is " + bankAccount.getBalance());
 
-        bankAccount.withdraw(accountHolder.getId(), MONEY_30$);
+        bankAccount.withdraw(accountHolder.getId(), WITHDRAW);
         System.out.println("Current balance is " + bankAccount.getBalance());
 
-        bankAccount.withdraw(accountHolder.getId(), MONEY_80$);
+        bankAccount.withdraw(accountHolder.getId(), WITHDRAW_TOO_HIGH);
         System.out.println("Current balance is " + bankAccount.getBalance());
     }
 }
